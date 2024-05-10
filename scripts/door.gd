bbbdaaf34e06c06f671726a7e057b0e82b43ffa8
r0ob3n1337 <r00b3n1337@gmail.com>
2024-05-10 19:33:05 +0300

    Add transition between scenes

--- scripts/door.gd
+++ scripts/door.gd
@@ -4,4 +4,6 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.name == "Player":
+		TransitionScrene.transition()
+		await TransitionScrene.on_transition_finished
 		get_tree().change_scene_to_file("res://scenes/" + next_scene_name + ".tscn")
