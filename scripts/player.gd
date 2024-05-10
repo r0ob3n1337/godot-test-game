bd3e956617cd255fc94d7b758b3534727c66b6fb
r0ob3n1337 <r00b3n1337@gmail.com>
2024-05-10 20:52:44 +0300

    Add level 3

--- scripts/player.gd
+++ scripts/player.gd
@@ -19,6 +19,10 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		jump_sound.play()
 		velocity.y = JUMP_VELOCITY
+		
+	# Handle down
+	if Input.is_action_just_pressed("move_down") and is_on_floor():
+		position.y += 1
 
 	# Get the input direction: -1 0 1
 	var direction = Input.get_axis("move_left", "move_right")
